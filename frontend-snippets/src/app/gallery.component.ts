2066758e24f82a70a0d3e536d8eac82375adeef2
ZEEWELC <zeewelc@gmail.com>
2025-09-02 18:11:22 +0100

    fire card

--- frontend-snippets/src/app/gallery.component.ts
+++ frontend-snippets/src/app/gallery.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
-  template: '<h2>Gallery Page</h2>',
-  styles: ['h2 { color: #7f6680; }']
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent { }
+
+
